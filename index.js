b04b7ad1f4d2f84380829c7dfb7ab93e8fc1c1a5
nqdai1992 <nguyendai1992@gmail.com>
2017-04-20 00:22:56 +0700

    First commit

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-/**
- * Created by Dainguyen on 18/04/2017.
- */
+var express = require('express')
+var app = express()
+var data = {}
+function getRandomNumber(min,max) {
+    return Math.floor(Math.random()*(max - min + 1) + min)
+}
+app.get('/new/*', function (req, res) {
+    var url = req.url;
+    var link = url.replace(/\/new\//gi,'');
+    var randomNumber = getRandomNumber(1000,9999);
+    data[randomNumber] = link;
+    res.send({
+        "original_url": link,
+        "short_url": req.protocol + '://' + req.get('host') + '/' + randomNumber
+    });
+});
+app.get('/:id', function (req, res) {
+    var id = req.params.id;
+    var linkInData = data[id] || null;
+    if(linkInData){
+        res.writeHead(301,{
+            Location: linkInData
+        })
+    }
+    res.end();
+})
+
+app.listen(3000);
